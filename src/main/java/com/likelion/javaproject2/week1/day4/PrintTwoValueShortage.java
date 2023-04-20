2e710176279401e61db362164027af9109c826b6
GrO0vy <lmc2819@naver.com>
2023-04-20 11:28:29 +0900

    feat: 구구단 출력까지 실습 - MultiplicationTable.java , PrintfEx.java , PrintTwoValueShortage.java, PrintWhy.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/day4/PrintTwoValueShortage.java
@@ -0,0 +1,7 @@
+package com.likelion.javaproject2.week1.day4;
+
+public class PrintTwoValueShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. 하고 싶은 말은 잠온다 입니다.","이민철","ㅂㅈㄷㅂㄷ");
+    }
+}
