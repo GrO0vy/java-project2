e304ba9336da91002042d7eec3cf732d88d098a4
GrO0vy <lmc2819@naver.com>
2023-04-20 10:38:26 +0900

    feat: print() 실습 - ScannerEx2.java , ScannerEx2Test.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/day2/ScannerEx2Test.java
@@ -0,0 +1,8 @@
+package com.likelion.javaproject2.week1.day2;
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScanenrEx2 scanenrEx2 =  new ScanenrEx2();
+        scanenrEx2.inputTwoNumbersPlus();
+    }
+}
