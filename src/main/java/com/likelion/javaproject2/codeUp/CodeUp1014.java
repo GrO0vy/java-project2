78e7ead0ba3c9c1c3a3013ec3ec716b78e55ecc9
GrO0vy <lmc2819@naver.com>
2023-04-20 15:33:13 +0900

    feat: CodeUp1014 문제 풀이 - CodeUp1014.java 추가

---
+++ src/main/java/com/likelion/javaproject2/codeUp/CodeUp1014.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1014 {
+    public static void main(String[] args)  {
+        String x,y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.next();
+        y = sc.next();
+
+        System.out.println(y+" "+x);
+
+    }
+}
