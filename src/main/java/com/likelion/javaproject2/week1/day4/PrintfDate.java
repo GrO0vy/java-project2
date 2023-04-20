23510b9aae4d0d65c4b89668ed593d998cb15ff9
GrO0vy <lmc2819@naver.com>
2023-04-20 11:48:37 +0900

    feat: 날짜 출력하기 실습 - PrintfDate.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/day4/PrintfDate.java
@@ -0,0 +1,9 @@
+package com.likelion.javaproject2.week1.day4;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n",4);
+        System.out.printf("%03d\n",34);
+        System.out.printf("%d-%02d-%02d",2023,4,20);
+    }
+}
