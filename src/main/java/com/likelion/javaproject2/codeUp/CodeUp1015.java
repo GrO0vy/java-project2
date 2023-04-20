001eb25f909e8a9b9f428b68cceec278f48722e8
GrO0vy <lmc2819@naver.com>
2023-04-20 15:49:10 +0900

    feat: CodeUp1015 문제 풀이 - CodeUp1015.java 추가

---
+++ src/main/java/com/likelion/javaproject2/codeUp/CodeUp1015.java
@@ -0,0 +1,11 @@
+package com.likelion.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextFloat();
+        System.out.printf("%.2f",num);
+    }
+}
