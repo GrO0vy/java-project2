bf4f4a4e9c95620cedf181393133ec6dd6a4bdec
GrO0vy <lmc2819@naver.com>
2023-04-21 14:42:19 +0900

    feat: Point OOP 실습 - Point.java, PointText.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/day5/Point.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week1.day5;
+
+public class Point {
+    int x;
+    int y;
+
+    boolean isSame(){
+        return x == y;
+    }
+}
--- src/main/java/com/likelion/javaproject2/week1/day5/SpaceInvaderMemberVariable.java
+++ src/main/java/com/likelion/javaproject2/week1/day5/SpaceInvaderMemberVariable.java
@@ -5,12 +5,12 @@ public class SpaceInvaderMemberVariable {
 
     public void moveLeft(){
         location = location - 1;
-        System.out.printf("moveLeft : %d\n" ,location);
+
     }
 
     public void moveRight(){
         location = location + 1;
-        System.out.printf("moveRight : %d\n",location);
+
     }
 
     public static void main(String[] args) {
@@ -21,5 +21,9 @@ public class SpaceInvaderMemberVariable {
         simv.moveRight();
         simv.moveRight();
         simv.moveRight();
+
+        System.out.println("최종위치 : " +simv.location);
+        simv.moveLeft();
+        System.out.println("최종위치 : " +simv.location);
     }
 }
