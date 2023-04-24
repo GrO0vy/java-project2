0a0e01b63cec3b908efed05135c1365d378f7c9a
GrO0vy <lmc2819@naver.com>
2023-04-24 11:29:16 +0900

    float 와 double 실습 - FloatAndDoubles.java,FloatDigit.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week2/FloatAndDoubles.java
@@ -0,0 +1,17 @@
+package com.likelion.javaproject2.week2;
+
+public class FloatAndDoubles {
+    public static void main(String[] args) {
+        float f1 = 3.14f;
+        float f2 = 1.23e10f;
+
+        System.out.printf("%f\n",f1);
+        System.out.printf("%f\n",f2);
+
+        double d1 = 3.14;
+        double d2 = 123e8;
+
+        System.out.printf("%f\n",d1);
+        System.out.printf("%f\n",d2);
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week2/FloatDigit.java
@@ -0,0 +1,16 @@
+package com.likelion.javaproject2.week2;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+        float f5 = 1.23e5f;
+        float f6 = 1.23e6f;
+        float f7 = 1.23e7f;
+        float f8 = 1.23E10f;
+        double d1 = 1.23E10;
+        System.out.printf("f5 : %f\n" ,f5);
+        System.out.printf("f6 : %f\n" ,f6);
+        System.out.printf("f7 : %f\n" ,f7);
+        System.out.printf("f8 : %f\n" ,f8);
+        System.out.printf("d1 : %f\n",d1);
+    }
+}
