04b8201f68db967ac489131f91f9f9e605867b6e
GrO0vy <lmc2819@naver.com>
2023-04-24 13:37:20 +0900

    feat : float parsing 까지 실습 - FloatNumParse,FloatPlus,NumberFormatException1,StringPlusInt,FloatPlus.java 추가

--- src/main/java/com/likelion/javaproject2/week2/FloatPlus.java
+++ src/main/java/com/likelion/javaproject2/week2/FloatPlus.java
@@ -1,2 +1,11 @@
-package com.likelion.javaproject2.week2;public class FloatPlus {
+package com.likelion.javaproject2.week2;
+
+public class FloatPlus {
+    public static void main(String[] args) {
+        String val1 = "1.1111111111111";
+        String val2 = "2.2222222222222";
+
+        float result = Float.parseFloat(val1)+ Float.parseFloat(val2);
+        System.out.println(result);
+    }
 }
