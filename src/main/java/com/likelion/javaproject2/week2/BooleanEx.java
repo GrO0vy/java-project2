0bf6c22f69f20b200a0ad132a473eda7d962423b
GrO0vy <lmc2819@naver.com>
2023-04-24 14:37:18 +0900

    feat : boolean 타입 변수선언 실습 - BooleanEx.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week2/BooleanEx.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.week2;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        boolean isPaymentSuccess = false;
+        boolean isBalanceSufficient = true;
+
+        System.out.println("isBalanceSufficient = " +isBalanceSufficient);
+        System.out.println("isPaymentSuccess = " +isPaymentSuccess);
+    }
+}
