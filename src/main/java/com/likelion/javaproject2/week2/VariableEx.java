7dec0061cf42c61a02940f0f79a67732aef7fe0c
GrO0vy <lmc2819@naver.com>
2023-04-24 10:11:15 +0900

    타입 3가지 출력 실습 - VariableEx.java 추가

--- src/main/java/com/likelion/javaproject2/week2/VariableEx.java
+++ src/main/java/com/likelion/javaproject2/week2/VariableEx.java
@@ -1,2 +1,11 @@
-package com.likelion.javaproject2.week2;public class VariableEx {
+package com.likelion.javaproject2.week2;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+
+        System.out.printf("String : %s Number : %d Float : %f",sOne,iOne,fOne);
+    }
 }
