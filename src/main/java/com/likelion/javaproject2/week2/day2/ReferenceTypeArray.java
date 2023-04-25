46cd699701a0e9ff8051a3b2ed212757c4483b6a
GrO0vy <lmc2819@naver.com>
2023-04-25 15:05:34 +0900

    feat : 참조타입 배열선언과 null 실습 - ReferenceTypeArray.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week2/day2/ReferenceTypeArray.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day2;
+
+import java.util.List;
+
+public class ReferenceTypeArray {
+    public static void main(String[] args) {
+        String[] sArr = new String[3];
+        System.out.println(sArr[0]);
+    }
+}
