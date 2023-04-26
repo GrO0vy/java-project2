5208a447fdc71a0be50a557fd78988645543c9db
GrO0vy <lmc2819@naver.com>
2023-04-26 11:35:19 +0900

    feat : 반복 작업 메서드로 리팩토링 실습 - TwoDimArrEx.java 추가

--- src/main/java/com/likelion/javaproject2/week2/day3/TwoDimArrEx.java
+++ src/main/java/com/likelion/javaproject2/week2/day3/TwoDimArrEx.java
@@ -1,2 +1,40 @@
-package com.likelion.javaproject2.week2.day3;public class TwoDimArrEx {
+package com.likelion.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class TwoDimArrEx {
+    public static void printArray(int[][] arr){
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+
+//        System.out.println(Arrays.toString(arr[0]));
+//        System.out.println(Arrays.toString(arr[1]));
+//        System.out.println(Arrays.toString(arr[2]));
+
+        printArray(arr);
+
+        arr[0][0] =1;
+        arr[0][2] =2;
+        arr[1][0] =3;
+
+//        System.out.println(Arrays.toString(arr[0]));
+//        System.out.println(Arrays.toString(arr[1]));
+//        System.out.println(Arrays.toString(arr[2]));
+
+        printArray(arr);
+
+        arr[1][1] = 5;
+
+//        System.out.println(Arrays.toString(arr[0]));
+//        System.out.println(Arrays.toString(arr[1]));
+//        System.out.println(Arrays.toString(arr[2]));
+
+        printArray(arr);
+
+    }
 }
