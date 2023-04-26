69d019f02aaadb5ddd8fcbd3bfe43fab10527900
GrO0vy <lmc2819@naver.com>
2023-04-26 15:50:39 +0900

    feat : Accumulate687.java , StringAccu.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week2/day3/Accumulate687.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day3;
+
+public class Accumulate687 {
+    public static void main(String[] args) {
+        int num = 0;
+        int answer = 0;
+
+
+    }
+}
