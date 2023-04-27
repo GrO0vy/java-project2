c274492ee91b1c2bce10ba02ff7a77ee94ac62d4
GrO0vy <lmc2819@naver.com>
2023-04-27 09:43:44 +0900

    feat : 배열에 있는 모든 값 더하기 실습 - ArraySum..java , SumOfValue.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week2/day4/ArraySum.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int answer = 0 ;
+        int[] arr = new int[]{2,1,7,9};
+        answer = answer +arr[0];
+        answer = answer +arr[1];
+        answer = answer +arr[2];
+        answer = answer +arr[3];
+
+        System.out.println("합 : " +answer);
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week2/day4/SumOfValue.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class SumOfValue {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,1,7,9};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+
+        System.out.printf("합 : %d",arr[0]+arr[1]+arr[2]+arr[3]);
+    }
+}
