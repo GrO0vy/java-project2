612b94b7524a84bb212956d7d3f0fec7bc41a331
GrO0vy <lmc2819@naver.com>
2023-04-27 09:27:54 +0900

    feat : 687자릿수 더하기 실습 - Accumulate687.java 추가

--- src/main/java/com/likelion/javaproject2/week2/day3/Accumulate687.java
+++ src/main/java/com/likelion/javaproject2/week2/day3/Accumulate687.java
@@ -2,9 +2,22 @@ package com.likelion.javaproject2.week2.day3;
 
 public class Accumulate687 {
     public static void main(String[] args) {
-        int num = 0;
+        int num = 687;
         int answer = 0;
 
+        answer = answer + (num%10);
+        num = num / 10;
 
+        System.out.printf("num : %d , answer : %d\n",num,answer);
+
+        answer = answer + (num%10);
+        num = num / 10;
+
+        System.out.printf("num : %d , answer : %d\n",num,answer);
+
+        answer = answer + (num%10);
+        num = num / 10;
+
+        System.out.printf("num : %d , answer : %d\n",num,answer);
     }
 }
