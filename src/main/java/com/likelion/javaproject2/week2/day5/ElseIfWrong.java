b62a6e7dc20a13cebab1eadc4b40fb8ae6ffe301
GrO0vy <lmc2819@naver.com>
2023-04-28 10:42:28 +0900

    feat : if 조건 논리연산자 실습 , 팀장 승진여부 코드 실습 - IfWithLogicalOperator , promotion .java 추가

--- src/main/java/com/likelion/javaproject2/week2/day5/ElseIfWrong.java
+++ src/main/java/com/likelion/javaproject2/week2/day5/ElseIfWrong.java
@@ -1,2 +1,18 @@
-package com.likelion.javaproject2.week2.day5;public class ElseIfWrong {
+package com.likelion.javaproject2.week2.day5;
+
+public class ElseIfWrong {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >=70){
+            System.out.println("C");
+        } else if (score >=80){
+            System.out.println("B");
+        } else if (score >=90){
+            System.out.println("A");
+        }
+        else{
+            System.out.println("F");
+        }
+    }
 }
