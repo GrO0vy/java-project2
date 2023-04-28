148e355730362eb834531b8afce0ffad054bedc0
GrO0vy <lmc2819@naver.com>
2023-04-28 14:13:56 +0900

    feat : CodeUp1116 ~ 1118 문제 풀이 - CodeUp1116 ~ 1118 .java 추가

---
+++ src/main/java/com/likelion/javaproject2/codeUp/CodeUp1117.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1117 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+
+        System.out.printf("%.2f",num1*num2);
+        sc.close();
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/codeUp/CodeUp1118.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int width = sc.nextInt();
+        int height = sc.nextInt();
+
+        System.out.printf("%.1f",width * height /2.0);
+    }
+}
