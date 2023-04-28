2afab1077ad6626a79a2023a203377a331d221a1
GrO0vy <lmc2819@naver.com>
2023-04-28 20:18:32 +0900

    feat : 명예의 전당 문제 중간 저장 - HallOfFame , HallOfFameTest .java 추가

--- src/main/java/com/likelion/javaproject2/week2/day5/HallOfFameTest.java
+++ src/main/java/com/likelion/javaproject2/week2/day5/HallOfFameTest.java
@@ -1,2 +1,10 @@
-package com.likelion.javaproject2.week2.day5;public class HallOfFameTest {
+package com.likelion.javaproject2.week2.day5;
+
+public class HallOfFameTest {
+    public static void main(String[] args) {
+        HallOfFame hallOfFame = new HallOfFame();
+
+        int[] arr = new int[]{10,100,20,150,1,100,200};
+        System.out.println(hallOfFame.solution(3,arr));
+    }
 }
