577d1ab8364b4a7cc3e7db9570b543c873c87d06
GrO0vy <lmc2819@naver.com>
2023-04-28 09:25:49 +0900

    feat : CodeUp1063 ~ 1067 문제 풀이 - CodeUp1063 ~ 1067 .java 추가

---
+++ src/main/java/com/likelion/javaproject2/codeUp/CodeUp1063.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1>num2 ? num1:num2);
+        sc.close();
+    }
+}
