825fb577a666938a263428d3fa8d824831a7c24c
GrO0vy <lmc2819@naver.com>
2023-05-01 09:38:04 +0900

    feat : NewSwitchCaseDaysOfMonth 실습 - NewSwitchCaseDaysOfMonth.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week3/day1/NewSwitchCaseDaysOfMonth.java
@@ -0,0 +1,16 @@
+package com.likelion.javaproject2.week3.day1;
+
+public class NewSwitchCaseDaysOfMonth {
+    public static void main(String[] args) {
+        int month =11;
+
+        int lastDate = switch (month){
+            case 1, 3, 5, 7, 8,10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> 28;
+            default -> throw new IllegalArgumentException("해당 월은 없습니다. 입력 월 : " +month );
+        };
+
+        System.out.printf("%d 월은 %d일까지 있습니다.",month,lastDate);
+    }
+}
