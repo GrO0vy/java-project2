f0d8cb683c5a2dc398a9afa97dfc6ad018a4e7a8
GrO0vy <lmc2819@naver.com>
2023-05-01 11:38:07 +0900

    feat : CodeUp1073 ~ CodeUp1075 문제 풀이 - CodeUp1073 ~ 1075 .java 추가

---
+++ src/main/java/com/likelion/javaproject2/codeUp/CodeUp1073.java
@@ -0,0 +1,17 @@
+package com.likelion.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1073 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+
+        while(true){
+            int num = sc.nextInt();
+            if(num ==0) break;
+            System.out.println(num);
+        }
+
+        sc.close();
+    }
+}
