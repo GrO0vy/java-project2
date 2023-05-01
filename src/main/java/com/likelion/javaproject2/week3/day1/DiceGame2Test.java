a2e9dff504391842b50d76051b9365aed77b112a
GrO0vy <lmc2819@naver.com>
2023-05-01 10:51:17 +0900

    feat : DiceGame2 문제 풀이 - DiceGame2 , DiceGame2Test .java 추가

---
+++ src/main/java/com/likelion/javaproject2/week3/day1/DiceGame2Test.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week3.day1;
+
+public class DiceGame2Test {
+    public static void main(String[] args) {
+        DiceGame2 dg2 = new DiceGame2();
+        System.out.println(dg2.solution(1,2,3));
+        System.out.println(dg2.solution(1,1,2));
+        System.out.println(dg2.solution(1,1,1));
+    }
+}
