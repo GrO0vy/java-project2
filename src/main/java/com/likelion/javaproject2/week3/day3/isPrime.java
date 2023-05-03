d66c2eaeab230d9a0c5dd45b3eeca1bc97297093
GrO0vy <lmc2819@naver.com>
2023-05-03 11:47:29 +0900

    feat : 소수인지 판별 실습 - IsPrime.java 추가

---
+++ src/main/java/com/likelion/javaproject2/week3/day3/isPrime.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.week3.day3;
+
+public class isPrime {
+    public static void main(String[] args) {
+        int num = 6;
+        int factors = 0;
+
+        for (int i = 2; i < num; i++){
+            if(num % i == 0) factors++;
+        }
+
+        System.out.printf("factors : %d\n",factors);
+        System.out.println(factors ==0 ? "소수입니다" : "소수가 아닙니다.");
+    }
+}
