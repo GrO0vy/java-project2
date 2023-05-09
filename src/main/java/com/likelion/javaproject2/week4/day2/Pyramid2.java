05f61be602f17d6dc5003b92c81396c3df9ac559
GrO0vy <lmc2819@naver.com>
2023-05-09 09:50:31 +0900

    fix : Pyramid2.java 수정 - 피라미드 출력 메소드를 피라미드 한 줄을 리턴하는 makeAline 메소드와 최종적으로 모든 줄을 반복해서 출력하는 printPyramidWithSpaceChar 메소드 추가

--- src/main/java/com/likelion/javaproject2/week4/day2/Pyramid2.java
+++ src/main/java/com/likelion/javaproject2/week4/day2/Pyramid2.java
@@ -9,10 +9,14 @@ public class Pyramid2 {
         this.spaceChar = spaceChar;
     }
 
-    public void printPyramid(int height){
+    public void printPyramidWithSpaceChar(int height){
         for(int i=0; i<height; i++){
-            System.out.println(spaceChar.repeat(height-i-1)+"*".repeat(2*i+1));
+            System.out.println(makeALine(height,i));
         }
+    }
+
+    public String makeALine(int height, int i){
+        return String.format("%s%s",spaceChar.repeat(height-i-1),"*".repeat(2*i+1));
     }
 
     public static void main(String[] args) {
@@ -21,8 +25,8 @@ public class Pyramid2 {
         Pyramid2 pyramid2 = new Pyramid2(" ");
         int height = sc.nextInt();
 
-        pyramid.printPyramid(height);
+        pyramid.printPyramidWithSpaceChar(height);
         System.out.println("=========================");
-        pyramid2.printPyramid(height);
+        pyramid2.printPyramidWithSpaceChar(height);
     }
 }
