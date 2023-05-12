17b1bc517491db00c8d387268ef5656a781a76ea
GrO0vy <lmc2819@naver.com>
2023-05-12 10:30:07 +0900

    feat : 파일 10줄만 불러오기 - ReadFileEx, LineReader .java 추가

--- src/main/java/com/likelion/javaproject2/week4/day5/ReadFileEx.java
+++ src/main/java/com/likelion/javaproject2/week4/day5/ReadFileEx.java
@@ -1,12 +1,34 @@
 package com.likelion.javaproject2.week4.day5;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.List;
 
-public class ReadFileEx {
+public class ReadFileEx implements LineReader {
+    LineReader lineReader;
+    String charSet;
+
+    public ReadFileEx(String charSet){
+        this.charSet = charSet;
+    }
+    public List<String> getLines(String filename) throws IOException {
+        List<String> lines = new LinkedList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(filename, Charset.forName(charSet)));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("./hospital.csv"));
-        System.out.println(br.readLine());
+        ReadFileEx rfe = new ReadFileEx("UTF-8");
+        List<String> result = rfe.getLines("hospital_info_10lines.csv");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(result.get(i));
+        }
     }
 }
