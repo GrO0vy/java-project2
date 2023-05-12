5e2e66473e2d323f958ed7bbe8fcf4ad70d5a8e1
GrO0vy <lmc2819@naver.com>
2023-05-12 21:13:08 +0900

    feat : 4week-5day task

--- src/main/java/com/likelion/javaproject2/week4/day4/Hospital.java
+++ src/main/java/com/likelion/javaproject2/week4/day4/Hospital.java
@@ -2,12 +2,36 @@ package com.likelion.javaproject2.week4.day4;
 
 public class Hospital {
     private String name;
-    private String phoneNumber;
+    private String websiteAddress;
     private Address address;
 
-    public Hospital(String name, String phoneNumber, Address address) {
+    public Hospital(String name, String websiteAddress, Address address) {
         this.name = name;
-        this.phoneNumber = phoneNumber;
+        this.websiteAddress = websiteAddress;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsiteAddress() {
+        return websiteAddress;
+    }
+
+    public void setWebsiteAddress(String phoneNumber) {
+        this.websiteAddress = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
         this.address = address;
     }
 }
--- src/main/java/com/likelion/javaproject2/week4/day5/ReadFileEx.java
+++ src/main/java/com/likelion/javaproject2/week4/day5/ReadFileEx.java
@@ -1,21 +1,26 @@
 package com.likelion.javaproject2.week4.day5;
 
+import com.likelion.javaproject2.week4.day4.Address;
+import com.likelion.javaproject2.week4.day4.Hospital;
+
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ReadFileEx implements LineReader {
     LineReader lineReader;
-    String charSet;
+    Charset charset;
 
-    public ReadFileEx(String charSet){
-        this.charSet = charSet;
+    public ReadFileEx(Charset charset){
+        this.charset = charset;
     }
     public List<String> getLines(String filename) throws IOException {
         List<String> lines = new LinkedList<>();
 
-        BufferedReader br = new BufferedReader(new FileReader(filename, Charset.forName(charSet)));
+        BufferedReader br = new BufferedReader(new FileReader(filename, charset));
 
         String line;
         while ((line = br.readLine()) != null) {
@@ -23,12 +28,27 @@ public class ReadFileEx implements LineReader {
         }
         return lines;
     }
+    public Hospital parse(String str){
+        String[] splitted = str.split(",");
 
-    public static void main(String[] args) throws IOException {
-        ReadFileEx rfe = new ReadFileEx("UTF-8");
-        List<String> result = rfe.getLines("hospital_info_10lines.csv");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(result.get(i));
+        Address address = new Address(splitted[10],splitted[5],splitted[7]);
+        Hospital hospital = new Hospital(splitted[1], splitted[11],address);
+
+        return hospital;
+    }
+
+    public List<Hospital> getHospital(List<String> lines){
+        List<Hospital> hospitals = new ArrayList<>();
+        for(String line : lines){
+            hospitals.add(parse(line));
         }
+        return hospitals;
+    }
+    public static void main(String[] args) throws IOException {
+        ReadFileEx rfe = new ReadFileEx(Charset.forName("UTF-8"));
+        List<String> strLines = rfe.getLines("hospital_info_10lines.csv");
+        List<Hospital> parsedHospital = rfe.getHospital(strLines);
+        System.out.println(parsedHospital.get(1).getName());
+
     }
 }
