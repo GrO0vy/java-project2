8d11a7de01352151456fc3e8d880e385f2d053be
GrO0vy <lmc2819@naver.com>
2023-05-15 10:51:02 +0900

    fix : sort() 메소드로 분리 - BubbleSort2.java 수정

--- src/main/java/com/likelion/javaproject2/algorithm/BubbleSort2.java
+++ src/main/java/com/likelion/javaproject2/algorithm/BubbleSort2.java
@@ -1,15 +1,25 @@
 package com.likelion.javaproject2.algorithm;
 
-public class BubbleSort2 {
-    public static void main(String[] args) {
-        int[] arr = {7,2,3,9,28,11};
+import java.util.Arrays;
 
-        for(int i =1; i<arr.length; i++){
-            if(arr[0] > arr[i]){
-                int temp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = temp;
+public class BubbleSort2 {
+    public int[] sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j =i+1; j<arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[i] = temp;
+                }
             }
         }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7,2,3,9,28,11};
+        BubbleSort02 bubbleSort02 = new BubbleSort02();
+
+        System.out.println(Arrays.toString(bubbleSort02.sort(arr)));
     }
 }
