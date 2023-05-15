bcedb91da61c36bd68741a89584e9ea7481997ef
GrO0vy <lmc2819@naver.com>
2023-05-15 09:25:10 +0900

    fix : for,if 문으로 버블정렬 한 사이클 구현 - BubbleSort01.java 수정

--- src/main/java/com/likelion/javaproject2/algorithm/BubbleSort01.java
+++ src/main/java/com/likelion/javaproject2/algorithm/BubbleSort01.java
@@ -1,13 +1,18 @@
 package com.likelion.javaproject2.algorithm;
 
+import java.util.Arrays;
+
 public class BubbleSort01 {
     public static void main(String[] args) {
-        int[] arr = {7, 2, 3, 9, 4, 3};
+        int[] arr = {7, 2, 3, 9, 28, 1};
 
-        int temp = arr[0];
-        arr[0] = arr[1];
-        arr[1] = temp;
-
-        System.out.println(arr[0] + " " + arr[1]);
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
