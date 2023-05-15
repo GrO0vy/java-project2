0ca39ba4fa1ebd0ea88d193aebb847c60d4bb53f
GrO0vy <lmc2819@naver.com>
2023-05-15 10:17:19 +0900

    fix : OOP 적용( sort() 메소드 추가 ) - BubbleSort.java 추가

--- src/main/java/com/likelion/javaproject2/algorithm/BubbleSort02.java
+++ src/main/java/com/likelion/javaproject2/algorithm/BubbleSort02.java
@@ -3,22 +3,30 @@ package com.likelion.javaproject2.algorithm;
 import java.util.Arrays;
 
 public class BubbleSort02 {
-    public int[] sortARound(int[] arr){
-        for (int i = 0; i < arr.length-j; i++) {
+    public int[] sortARound(int[] arr, int until) {
+        for (int i = 0; i < until; i++) {
             if (arr[i] > arr[i + 1]) {
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
             }
         }
-
+        return arr;
     }
+
+    public int[] sort(int[] arr){
+        for(int i=1; i<arr.length; i++){
+            sortARound(arr,arr.length-i);
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         BubbleSort02 bubbleSort02 = new BubbleSort02();
         int[] arr = {7, 2, 3, 9, 28, 1};
 
         for(int j=1; j<arr.length; j++){
-            bubbleSort02.sortARound(arr);
+            bubbleSort02.sort(arr);
         }
         System.out.println(Arrays.toString(arr));
     }
