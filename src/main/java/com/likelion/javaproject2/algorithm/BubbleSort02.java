68f05b9d20dbb7392a2963aa70758bdb99a1ad04
GrO0vy <lmc2819@naver.com>
2023-05-15 09:49:42 +0900

    fix : 버블정렬 OOP로 구현(sortARound 메소드 추가) - BublleSort02.java 수정

--- src/main/java/com/likelion/javaproject2/algorithm/BubbleSort02.java
+++ src/main/java/com/likelion/javaproject2/algorithm/BubbleSort02.java
@@ -3,17 +3,22 @@ package com.likelion.javaproject2.algorithm;
 import java.util.Arrays;
 
 public class BubbleSort02 {
+    public int[] sortARound(int[] arr){
+        for (int i = 0; i < arr.length-j; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+    }
     public static void main(String[] args) {
+        BubbleSort02 bubbleSort02 = new BubbleSort02();
         int[] arr = {7, 2, 3, 9, 28, 1};
 
         for(int j=1; j<arr.length; j++){
-            for (int i = 0; i < arr.length-j; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    int temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
-                }
-            }
+            bubbleSort02.sortARound(arr);
         }
         System.out.println(Arrays.toString(arr));
     }
