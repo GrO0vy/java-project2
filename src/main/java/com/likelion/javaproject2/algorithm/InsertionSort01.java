d711f4241e123778f7dd306bdbafc7ab26ec3138
GrO0vy <lmc2819@naver.com>
2023-05-16 09:26:22 +0900

    삽입정렬 step1 - InsertionSort01.java 추가

---
+++ src/main/java/com/likelion/javaproject2/algorithm/InsertionSort01.java
@@ -0,0 +1,13 @@
+package com.likelion.javaproject2.algorithm;
+
+public class InsertionSort01 {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 3, 9, 28, 11};
+
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = i; j>0; j--){
+                System.out.printf("i: %d , j: %d , j-1 : %d\n",i,j,j-1);
+            }
+        }
+    }
+}
