2fbbc701f42b1efbe41f55df57bd20d21e4929c7
GrO0vy <lmc2819@naver.com>
2023-05-16 10:25:08 +0900

    feat : 삽입정렬에 숫자, 알파벳 정렬 적용 - InsertionSortGeneric.java 추가

---
+++ src/main/java/com/likelion/javaproject2/algorithm/InsertionSortGeneric.java
@@ -0,0 +1,53 @@
+package com.likelion.javaproject2.algorithm;
+
+import java.util.Arrays;
+
+public class InsertionSortGeneric {
+
+    public Character[] sort(Character[] arr, boolean isAscending){
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = i; j>0; j--){
+                if((isAscending ? (arr[j-1] - arr[j] ) : (arr[j] - arr[j-1] )) > 0){
+                    Character temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public Character[] sort(Character[] arr){
+        return sort(arr,true);
+    }
+
+    public int[] sort(int[] arr , boolean isAscending){
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = i; j>0; j--){
+                if((isAscending ? (arr[j-1] - arr[j] ) : (arr[j] - arr[j-1] )) > 0){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[] sort(int[] arr){
+        return sort(arr, true);
+    }
+    public static void main(String[] args) {
+        InsertionSortGeneric is = new InsertionSortGeneric();
+        //int[] arr = {7, 2, 3, 9, 28, 11};
+        Character[] cArr = {'f','e','g','a','c'};
+        cArr = is.sort(cArr);
+        System.out.println(Arrays.toString(cArr));
+
+        //System.out.println(Arrays.toString(is.sort(arr,true)));
+    }
+}
