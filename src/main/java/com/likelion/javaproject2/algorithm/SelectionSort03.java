d2f0526c94854d095389197c15bf87445c8b79b1
GrO0vy <lmc2819@naver.com>
2023-05-19 23:13:20 +0900

    feat : 선택정렬 구현 - SelectionSort03.java 추가

---
+++ src/main/java/com/likelion/javaproject2/algorithm/SelectionSort03.java
@@ -0,0 +1,26 @@
+package com.likelion.javaproject2.algorithm;
+
+import java.util.Arrays;
+
+public class SelectionSort03 {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 3, 9, 28, 11};
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int targetValue = arr[i];
+            int targetIdx = i;
+
+            for (int j = i; j < arr.length; j++) {
+                if (targetValue > arr[j]) {
+                    targetValue = arr[j];
+                    targetIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[targetIdx];
+            arr[targetIdx] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
