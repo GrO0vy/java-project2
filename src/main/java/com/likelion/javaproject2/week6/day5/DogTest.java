70b4b7ac749fc28f3bc0d40b4cc2a8ac3f887897
GrO0vy <lmc2819@naver.com>
2023-05-26 11:30:54 +0900

    feat : 추상클래스 실습 - Animal, Dog, DogTest .java 추가

---
+++ src/main/java/com/likelion/javaproject2/week6/day5/DogTest.java
@@ -0,0 +1,9 @@
+package com.likelion.javaproject2.week6.day5;
+
+public class DogTest {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.makeSound();
+        dog.eat();
+    }
+}
