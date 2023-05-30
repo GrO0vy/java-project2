62c97f71c0d7c291fbaad962cf7b463bb4fe3864
GrO0vy <lmc2819@naver.com>
2023-05-30 11:32:23 +0900

    fix : LeapYear.java 수정

--- src/main/java/com/likelion/javaproject2/week7/day1/LeapYear.java
+++ src/main/java/com/likelion/javaproject2/week7/day1/LeapYear.java
@@ -6,19 +6,13 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
-        checkLeapYear(year);
+
+        if(checkLeapYear(year)) System.out.println(year + " O");
+        else System.out.println(year + " X");
     }
 
     public static boolean checkLeapYear(int year){
-        if( (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) ) {
-            System.out.printf("%d O\n", year);
-            return true;
-        }
-        else {
-            System.out.printf("%d X\n", year);
-            return false;
-        }
-
+        return  (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
     }
 
 }
